569021fd5ad6e82792077cfd480942fbf54af14f
Dani Ortiz <danielorrami@hotmail.com>
2024-10-17 16:49:25 -0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => {
                     </div>
                     
                     <h2 className="headline-1 max-w-[15ch] sm:max-w-[20ch] lg:max-w-[18ch] mt-5 mb-8 lg:mb-10">
-                        Creando soluciones y diseños únicos e innovadores.
+                        Hola! Soy Dani. Disarrollador Web, Diseñador 3D y Analista en Sistemas.
                     </h2>
 
                     <div className="flex items-center gap-3">
